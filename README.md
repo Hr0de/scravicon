c1e1f6ffbfa2f23329244bb2c94d8c4019839844
Hrode <pro@hrode.fr>
2021-08-09 21:59:13 +0200

    dev

--- README.md
+++ README.md
@@ -1,11 +1,40 @@
+
 # scravicon
 
 > Extract URL's favicons
 
-Usage
+Example of usage
 
     const scravicon = require('scravicon');
-    scravicon(url).then(function (favicons) {
-        // TODO
+    scravicon("https://www.alsacreations.com/astuce/lire/59-icon-link-rel-favicon-ico-navigateur.html", {
+        selector: ""
+    }).then((favicons) => {
+        console.log(favicons);
+        /* [
+		{
+			href: 'https://cdn.alsacreations.net/favicon-32x32.png',
+			height: 32, // can be missing
+			width: 32, // can be missing
+			type: 'image/png' // can be missing
+		},
+		{
+			href: 'https://cdn.alsacreations.net/android-chrome-192x192.png',
+			height: 192,
+			width: 192,
+			type: 'image/png'
+		},
+		{
+			href: 'https://cdn.alsacreations.net/favicon-96x96.png',
+			height: 96,
+			width: 96,
+			type: 'image/png'
+		},
+		{
+			href: 'https://cdn.alsacreations.net/favicon-16x16.png',
+			height: 16,
+			width: 16,
+			type: 'image/png'
+		}
+	] */
     });
 
--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const {JSDOM} = require("jsdom");
 
-module.exports = function (url) {
+module.exports = function (url, params) {
+    if (!(typeof params.selector === "string" && params.selector.length)) {
+        params.selector = 'head > link[rel="icon"], head > link[rel="shortcut icon"]';
+    }
     return new Promise((resolve, reject) => {
+        var favicons = [];
         JSDOM.fromURL(url, {}).then(dom => {
-            dom.window.document.querySelectorAll('head > link[rel="icon"]').forEach(link => {
-                console.log(link.href);
+            dom.window.document.querySelectorAll(params.selector).forEach(link => {
+                var favicon = {
+                    href: link.href
+                };
+
+                const attributes = link.attributes;
+
+                if (attributes.getNamedItem("sizes") !== null) {
+                    var sizes = attributes.getNamedItem("sizes").value.split('x');
+                    // TODO check sizes array de 2 integers
+                    favicon.height = parseInt(sizes[0]);
+                    favicon.width = parseInt(sizes[1]);
+                }
+
+                if (attributes.getNamedItem("type") !== null) {
+                    favicon.type = attributes.getNamedItem("type").value;
+                }
+
+                favicons.push(favicon);
             });
+            resolve(favicons);
         });
-        resolve([]);
     });
 };
